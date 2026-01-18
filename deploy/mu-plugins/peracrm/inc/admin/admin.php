2f3ca99c2feb13dde794c2a17c871ec7cab2f711
7mus7y-dotcom <7mus7y@gmail.com>
2026-01-18 21:06:35 +0300

    Add pipeline bulk actions

--- deploy/mu-plugins/peracrm/inc/admin/admin.php
+++ deploy/mu-plugins/peracrm/inc/admin/admin.php
@@ -23,6 +23,7 @@ add_action('admin_post_peracrm_reassign_client_advisor', 'peracrm_handle_reassig
 add_action('admin_post_peracrm_pipeline_save_view', 'peracrm_handle_pipeline_save_view');
 add_action('admin_post_peracrm_pipeline_delete_view', 'peracrm_handle_pipeline_delete_view');
 add_action('admin_post_peracrm_pipeline_move_stage', 'peracrm_handle_pipeline_move_stage');
+add_action('admin_post_peracrm_pipeline_bulk_action', 'peracrm_handle_pipeline_bulk_action');
 
 add_action('admin_notices', 'peracrm_admin_notices');
 add_action('admin_enqueue_scripts', 'peracrm_admin_enqueue_assets');
